3b23423663903edb549a05645439bdab0e36f331
Enrique Verea <kikeverea@gmail.com>
2025-03-17 02:39:02 +0100

    Add basic functionality to select component

--- src/components/Select/Select.test.tsx
+++ src/components/Select/Select.test.tsx
@@ -1,67 +1,156 @@
 import { render, screen } from '@testing-library/react'
 import Select, { Option } from './Select.tsx'
-import userEvent from '@testing-library/user-event'
+import { userEvent, UserEvent } from '@testing-library/user-event'
 
 
 describe('Select', () => {
 
-  let options: Option[]
+  const options: Option[] = [
+    { name: "Option 1", value: "option-1" },
+    { name: "Option 2", value: "option-2" },
+    { name: "Option 3", value: "option-3" }
+  ]
 
-  beforeEach(() => {
-    options = options = [
-      { name: "Option 1", value: "option-1" },
-      { name: "Option 2", value: "option-2" },
-      { name: "Option 3", value: "option-3" }
-    ]
-  })
+  let selectElement: HTMLElement
 
-  test('Without options, renders an empty select', () => {
-    render(<Select options={[]}/>)
+  describe('With no options', () => {
+    test('Without options, renders empty message', () => {
+      render(<Select options={[]} isExpanded={ true }/>)
 
-    const optionElements = screen.getAllByRole('option')
+      const optionElements = screen.getAllByRole('option')
 
-    expect(optionElements.length).toBe(1)
-    expect(optionElements[0].textContent).toBe('No data available')
-  })
-
-  test('With options, renders options', () => {
-
-    render(<Select options={ options }/>)
-
-    const optionElements: HTMLOptionElement[] = screen.getAllByRole('option')
-    expect(optionElements.length).toBe(3)
-
-    optionElements.forEach((option, index) => {
-      expect(option.textContent).toBe(`${options[index].name}`)
-      expect(option.value).toBe(`${options[index].value}`)
+      expect(optionElements.length).toBe(1)
+      expect(optionElements[0].textContent).toBe('No data available')
     })
   })
 
-  test('With options, renders search input', () => {
-    render(<Select options={ options }/>)
+  describe('with no expanded select', () => {
+    beforeEach(async () => {
+      render(<Select options={ options } isExpanded={ false }/>)
+      selectElement = screen.getByRole('combobox')
+    })
 
-    const input: HTMLInputElement = screen.getByRole('searchbox')
-    expect(input.type).toBe('search')
+    test('dropdown list is not rendered by default', () => {
+      const dropdown = screen.queryByRole('listbox')
+
+      expect(dropdown).toBeNull()
+      expect(selectElement.ariaExpanded).toBe('false')
+    })
+
+    test('clicking on the select component shows the dropdown list', async () => {
+      const user = userEvent.setup()
+      await user.click(selectElement)
+
+      const dropdown = screen.queryByRole('listbox')
+      expect(dropdown).not.toBeNull()
+      expect(selectElement.ariaExpanded).toBe('true')
+    })
   })
 
-  test('filters options by search', () => {
-    const user = userEvent.setup()
-    let optionElements: HTMLOptionElement[]
+  describe('With expanded select', () => {
 
-    render(<Select options={ options }/>)
+    let user: UserEvent
 
-    const input: HTMLInputElement = screen.getByRole('searchbox')
+    beforeEach(async () => {
+      render(<Select options={ options } isExpanded={ true }/>)
+      selectElement = screen.getByRole('combobox')
+      user = userEvent.setup()
+    })
 
-    user.type(input, "option")
-    optionElements = screen.queryAllByRole('option')
-    expect(optionElements).toHaveLength(3)
+    test('With options, renders options', () => {
+      const optionElements: HTMLOptionElement[] = screen.getAllByRole('option')
+      expect(optionElements.length).toBe(3)
 
-    user.type(input, "1")
-    optionElements = screen.queryAllByRole('option')
-    expect(optionElements).toHaveLength(1)
+      optionElements.forEach((option, index) => {
+        expect(option.textContent).toBe(`${options[index].name}`)
+        expect(option.dataset.value).toBe(`${options[index].value}`)
+      })
+    })
 
-    // user.type(input, "11")
-    // optionElements = screen.queryAllByRole('option')
-    // expect(optionElements).toHaveLength(0)
+    test('Renders search input', () => {
+      const input: HTMLInputElement = screen.getByRole('searchbox')
+      expect(input.type).toBe('search')
+    })
+
+    test('filters options by search', async () => {
+      let optionElements: HTMLOptionElement[]
+
+      const input: HTMLInputElement = screen.getByRole('searchbox')
+
+      await user.type(input, "option")
+
+      optionElements = screen.queryAllByRole('option')
+      expect(optionElements).toHaveLength(3)
+
+      await user.type(input, " 1")
+      optionElements = screen.queryAllByRole('option')
+      expect(optionElements).toHaveLength(1)
+      expect(optionElements[0].textContent).toBe('Option 1')
+
+      await user.type(input, "11")
+      optionElements = screen.queryAllByRole('option')
+      expect(optionElements).toHaveLength(1)
+      expect(optionElements[0].textContent).toBe('No data available')
+    })
+
+    test('shows selected option', async () => {
+      const randIndex = randomNumber(options.length)
+      const optionElements = screen.queryAllByRole('option')
+
+      const randomOption = options[randIndex]
+      const randomOptionElement = optionElements[randIndex]
+
+      await user.click(randomOptionElement)
+
+      const selectedOptionName = screen.getByTestId('selected-option-name')
+      const selectedOptionValue: HTMLInputElement = screen.getByTestId('selected-option-value')
+
+      expect(selectedOptionName.textContent).toContain(randomOption.name)
+      expect(selectedOptionValue.value).toBe(randomOption.value)
+    })
+
+    test('selecting an option clears the search', async () => {
+      const optionElements = screen.queryAllByRole('option')
+      const randomOptionElement = optionElements[randomNumber(options.length)]
+      const searchBox: HTMLInputElement = screen.getByRole('searchbox')
+
+      await user.type(searchBox, 'option')
+      await user.click(randomOptionElement)
+
+      expect(searchBox.value).toBe('')
+    })
+
+    test('Without a selected option, clear selection button is not rendered', () => {
+      const clear = screen.queryByLabelText('clear selected option')
+      expect(clear).toBeNull()
+    })
+
+    test('With a selected option, clear selection button is rendered', async () => {
+      const optionElements = screen.queryAllByRole('option')
+      const randomOptionElement = optionElements[randomNumber(options.length)]
+      await user.click(randomOptionElement)
+
+      const clear = screen.queryByLabelText('clear selected option')
+      expect(clear).not.toBeNull()
+    })
+
+    test('clicking clear selection button, clears the selected option', async () => {
+      const randIndex = randomNumber(options.length)
+      const optionElements = screen.queryAllByRole('option')
+      const randomOptionElement = optionElements[randIndex]
+
+      await user.click(randomOptionElement)
+
+      const clear = screen.getByLabelText('clear selected option')
+      const selectedOptionName = screen.getByTestId('selected-option-name')
+      const selectedOptionValue: HTMLInputElement = screen.getByTestId('selected-option-value')
+
+      await user.click(clear)
+
+      expect(selectedOptionName.textContent).toBe('')
+      expect(selectedOptionValue.value).toBe('')
+    })
   })
 })
+
+const randomNumber = (max: number): number => Math.floor(Math.random() * max)
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import {FormEvent, useState} from 'react'
+import {MouseEvent, useState} from 'react'
 
 export type Option = {
   name: string
@@ -6,38 +6,85 @@ export type Option = {
 }
 
 type SelectProps = {
-  options: Option[]
+  options: Option[],
+  isExpanded?: boolean
 }
 
-const Select = ({ options } : SelectProps) => {
-  const [ search, setSearch ] = useState('')
+/*
+ Mouse Up/Down support -> aria-activedescendant
+*/
 
-  const filterOptions = (e: FormEvent<HTMLInputElement>) => {
-    console.log('filtering options with', e.currentTarget.value)
-    const input = e.currentTarget as HTMLInputElement
+const Select = ({ options, isExpanded } : SelectProps) => {
+  const [ search, setSearch ] = useState('')
+  const [ selectedOption, setSelectedOption ] = useState<Option | null>(null)
+  const [ expanded, setExpanded ] = useState(isExpanded)
+
+  const selectOption = (e: MouseEvent<HTMLDivElement>) => {
+    const option = e.currentTarget
+    setSelectedOption({ name: option.textContent || '', value: option.dataset.value || '' })
+    setSearch('')
+  }
+
+  const filterOptions = (e: MouseEvent<HTMLInputElement> ) => {
+    const input = e.currentTarget
     setSearch(input.value)
   }
 
-  const showSearch = options.length > 0
+  const expand = () => {
+    if (!expanded)
+      setExpanded(true)
+  }
+
+  const clearSelectedOption = () => setSelectedOption(null)
 
   const filteredOptions = options.filter(
     option => option.name.toLowerCase().includes(search.toLowerCase()))
 
   return (
-    <>
-      <select>
-       { filteredOptions.length > 0
-         ? filteredOptions.map(option =>
-           <option value={ option.value } key={ option.value }>{ option.name }</option>)
-         : <option>{ 'No data available' }</option>
-       }
-      </select>
-      { showSearch &&
-        <span>
-          <input type="search" onInput={ filterOptions }></input>
-        </span>
+    <div
+      role='combobox'
+      aria-haspopup='listbox'
+      aria-owns="dropdown-list"
+      aria-controls="dropdown-list"
+      aria-expanded={ expanded }
+      style={{ border: '1px solid white', minWidth: '250px', minHeight: '30px'}}
+      onClick={ expand }
+      tabIndex={ 0 }
+    >
+      { /* Selected Option */ }
+      <div id='selected-option' data-testid='selected-option-name' aria-live='polite'>
+        { selectedOption?.name }
+        { selectedOption &&
+          <span style={{fontSize: '12px'}} onClick={clearSelectedOption} role='button' aria-label='clear selected option'>
+            Clear
+          </span>
+        }
+      </div>
+      <input type="hidden" value={ selectedOption?.value || '' } data-testid='selected-option-value' />
+
+      { /* Dropdown */ }
+      { expanded &&
+        <div id='dropdown-list' role='listbox'>
+
+          <input type="search" onInput={ filterOptions } tabIndex={ 0 } value={ search }></input>
+
+          { filteredOptions.length > 0
+            ? filteredOptions.map(option =>
+              <div
+                key={ option.value }
+                role='option'
+                data-value={ option.value }
+                onClick={ selectOption }
+                tabIndex={ 0 }
+              >
+                { option.name }
+              </div>
+            )
+            : <div role='option'>{ 'No data available' }</div>
+          }
+        </div>
       }
-    </>
+    </div>
   )
 }
 
